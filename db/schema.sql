c8a55313eaccd343333a3bd6c1df2e765c83a928
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-17 19:30:16 -0500

    added role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE department (
     name VARCHAR(30) 
 );
 
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30), 
+    salary DECIMAL, 
+    department_id INT,   
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
