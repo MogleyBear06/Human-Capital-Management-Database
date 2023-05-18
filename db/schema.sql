844c23a456bb3da6a14c3d5aee26a6288eb34db6
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-17 19:32:22 -0500

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,15 @@ CREATE TABLE role (
     INDEX dep_ind (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30), 
+    role_id INT,  
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
