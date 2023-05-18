42ade2c460a5aab5a43fbb68827fbe1b1dfde0be
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-05-17 19:36:50 -0500

    added title, salary and dept. ID

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,15 @@ VALUES
     ('Directors'),
     ('Production'),
     ('Accounting');
+
+    INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Executive Producer', 300000, 1),
+    ('Associate Producer', 200000, 1),
+    ('Director', 300000, 2),
+    ('Assistant Director', 120000, 2),
+    ('Unit Production Manager', 160000, 3),
+    ('Production Coordinator', 60000, 3),
+    ('Production Accountant', 250000, 4),
+    ('First Ass Accountant', 125000, 4);
